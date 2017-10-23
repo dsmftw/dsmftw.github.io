aaaca9ea0ae98316e676b92b97ad1c0c598b988c
Igor Kozlov <ikozlov11@gmail.com>
2017-10-23 02:04:17 -0700

    updated cv page

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -4,8 +4,8 @@ import Link from 'gatsby-link'
 const CVPage = () => (
   <div>
     <h1>R&eacute;sum&eacute; / CV</h1>
-    <p><a href='https://github.com/dsmftw/resume/raw/master/ik-resume.pdf' download>Download</a> my r&eacute;sum&eacute;.</p>
-    <p>Also, feel free to explore the <a target="_blank" rel="nofollow" href='https://github.com/dsmftw/resume'>LaTeX source code</a> on my GitHub repository.</p>
+    <p><a href='https://github.com/dsmftw/resume/raw/master/ik-resume.pdf' download>Download</a> a copy of my r&eacute;sum&eacute;.</p>
+    <p>Also, feel free to explore its <a target="_blank" rel="nofollow" href='https://github.com/dsmftw/resume'><span className="latex">L<sup>a</sup>T<sub>e</sub>X</span> source code</a> on my GitHub repository.</p>
   </div>
 )
 
