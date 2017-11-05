b58c0bb2e3bbafa6289340de41a09da96b3337a8
Igor Kozlov <ikozlov11@gmail.com>
2017-11-05 01:15:35 -0700

    Updates

---
+++ component---src-pages-contact-js-52cfd44ff5dafe3caf3d.js
@@ -0,0 +1,2 @@
+webpackJsonp([70144966829960],{186:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var u=l(4),n=a(u),f=l(40),o=(a(f),"mailto:ikozlov@sfu.ca"),r=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"Contact"),n.default.createElement("p",null,"Feel free to email me directly at ",n.default.createElement("a",{href:o},"ikozlov@sfu.ca")))};t.default=r,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-contact-js-52cfd44ff5dafe3caf3d.js.map
